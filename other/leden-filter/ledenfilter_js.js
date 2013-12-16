a68b97b7cf54445804ea14bec0c75b2d004301a5
Rein <djrein86@hotmail.com>
2013-12-16 14:28:37 +0100

    IE bug

--- other/leden-filter/ledenfilter_js.js
+++ other/leden-filter/ledenfilter_js.js
@@ -1,5 +1,5 @@
 $(function(){
-	$('.ledenfilter-wrapper').ledenFilter();
+    $('.ledenfilter-wrapper').ledenFilter();
 });
 
 ;(function ( $, window, document, undefined ) {
@@ -40,13 +40,13 @@ $(function(){
         this.init();
     }
 
-	//init
+    //init
     Plugin.prototype.init = function () {
-    	//default vars
-    	var $elem = $(this.element),
-    		obj = this;
-    	
-    	//set the first one as active
+        //default vars
+        var $elem = $(this.element),
+            obj = this;
+        
+        //set the first one as active
         $elem.find(obj.options.tabClass + ' ul li:first').addClass(obj.options.activeTabClass);
         
         //set the onclick events
@@ -57,147 +57,152 @@ $(function(){
         
         //check if the user is logged_in
         if($(obj.options.loggedInClass).length) {
-        	obj.options.loggedIn = true;
+            obj.options.loggedIn = true;
         }
         
         //get the first selection "a"
         this.get('overview', 'a');
-		
+        
     };
     
     //Get a selection based on the char
-    Plugin.prototype.get = function (type, search_char, id) {
-    	//default vars
-    	var $elem = $(this.element),
-    		obj = this;
-    	
-    	var type = type || 'overview';
-    	var search_char = search_char || '';
-    	var id = id || '';
-    	
-    	//nothing for overview?
-    	if(type == 'overview' && search_char == '') {
-    		return;
-    	}
-    	
-    	//nothing for detail?
-    	if(type == 'detail' && search_char == '' && id == '') {
-    		return;
-    	}
-    	
-    	//nothing for search?
-    	if(type == 'search' && search_char == '') {
-    		return;
-    	}
-    	
-    	//set the url
-    	if(type == 'overview') {
-    		var url = obj.ajaxUrl+"/"+search_char;
-    	
-    	//detail
-    	} else if(type == 'detail') {
-    		var url = obj.ajaxUrl+"/"+search_char+"/"+id;
-    		
-    	//search	
-    	} else {
-    		var url = obj.ajaxUrl+"/search/"+search_char;
-    	}
+    Plugin.prototype.get = function (type, search_char, id, callback) {
+        //default vars
+        var $elem = $(this.element),
+            obj = this;
+        
+        var type = type || 'overview';
+        var search_char = search_char || '';
+        var id = id || '';
+        
+        //nothing for overview?
+        if(type == 'overview' && search_char == '') {
+            return;
+        }
+        
+        //nothing for detail?
+        if(type == 'detail' && search_char == '' && id == '') {
+            return;
+        }
+        
+        //nothing for search?
+        if(type == 'search' && search_char == '') {
+            return;
+        }
+        
+        //set the url
+        if(type == 'overview') {
+            var url = obj.ajaxUrl+"/"+search_char;
+        
+        //detail
+        } else if(type == 'detail') {
+            var url = obj.ajaxUrl+"/"+search_char+"/"+id;
+            
+        //search    
+        } else {
+            var url = obj.ajaxUrl+"/search/"+search_char;
+        }
 
-    	//place placeholder
-    	$(obj.options.resultDiv).html("<center><img src='"+obj.options.loader+"'></center>");
-    	
-    	//get the data
-    	//the timeout, so we can see the loader :-)
+        //place placeholder
+        $(obj.options.resultDiv).html("<center><img src='"+obj.options.loader+"'></center>");
+        
+        //get the data
+        //the timeout, so we can see the loader :-)
         setTimeout(function(){
-        	$.get(url, function(html){
-				$(obj.options.resultDiv).html(html);
-				$(obj.options.resultDiv).hide(); // voor de fadeIn
-				$(obj.options.resultDiv).attr('rel','a');
-				$(obj.options.resultDiv).fadeIn(300);
-				
-				//verwijder cursor:pointer wanneer je niet ingelogd bent.
-				if(obj.options.loggedIn == false && obj.options.restrict == true) {
-					$(obj.options.resultDiv + ' li a').css('cursor', 'default');	
-				}
-				
-				//set the detail click
-				if((obj.options.loggedIn == true && obj.options.restrict == true) || (obj.options.restrict == false)) {
-					obj.addDetailClick();
-				}
-			});
+            $.get(url, function(html){
+                $(obj.options.resultDiv).html(html);
+                $(obj.options.resultDiv).hide(); // voor de fadeIn
+                $(obj.options.resultDiv).attr('rel','a');
+                $(obj.options.resultDiv).fadeIn(300);
+                
+                //verwijder cursor:pointer wanneer je niet ingelogd bent.
+                if(obj.options.loggedIn == false && obj.options.restrict == true) {
+                    $(obj.options.resultDiv + ' li a').css('cursor', 'default');    
+                }
+                
+                //set the detail click
+                if((obj.options.loggedIn == true && obj.options.restrict == true) || (obj.options.restrict == false)) {
+                    obj.addDetailClick();
+                }
+                
+                //fire callback
+                if(typeof(callback) == 'function') {
+                    callback();
+                }
+            });
         }, 500);
     };
 
-	//add a onclick on the tabs
+    //add a onclick on the tabs
     Plugin.prototype.addTabClick = function () {
-    	//default vars
-    	var $elem = $(this.element),
-    		obj = this;
-    		
-    	//set the click
-    	$(document).on('click', obj.options.tabClass + ' ul li:not(.'+obj.options.activeTabClass+')', function(e){
-			e.preventDefault();
-			
-			//remove active state
-			$(obj.options.tabClass + ' ul li').removeClass(obj.options.activeTabClass);
-			$(this).addClass(obj.options.activeTabClass);
-			
-			//get the selection
-			obj.get('overview', $(this).data('char'));
-			
-			return false;
-		});
+        //default vars
+        var $elem = $(this.element),
+            obj = this;
+            
+        //set the click
+        $(obj.options.tabClass + ' ul li:not(.'+obj.options.activeTabClass+')').click(function(e){
+            e.preventDefault();
+            
+            //remove active state
+            $(obj.options.tabClass + ' ul li').removeClass(obj.options.activeTabClass);
+            $(this).addClass(obj.options.activeTabClass);
+            
+            //get the selection
+            obj.get('overview', $(this).data('char'));
+            
+            return false;
+        });
     };
     
     //add a onclick on the tabs
     Plugin.prototype.addDetailClick = function () {
-    	//default vars
-    	var $elem = $(this.element),
-    		obj = this;
-    	
-    	$(document).on('click', obj.options.resultDiv + ' li a', function(){
-    	
-	    	var id = $(this).children("input").attr("name");
-			var search_char = $(obj.options.tabClass + ' ul li.active').data('char');
-			
-			//get the detail
-			obj.get('detail', search_char, id);
-			
-			//add the click events for back
-			$(document).on('click','.member-detail-footer a', function(e){	
-				e.preventDefault();
-				
-				//get back to the overview	
-				obj.get('overview', search_char);
-			});
-			
-		});
+        //default vars
+        var $elem = $(this.element),
+            obj = this;
+
+        $(obj.options.resultDiv + ' li a').click(function(event){
+            event.stopPropagation();
+
+            var id = $(this).children("input").attr("name");
+            var search_char = $(obj.options.tabClass + ' ul li.active').data('char');
+            
+            //get the detail
+            obj.get('detail', search_char, id, function(){
+                //add the click events for back
+                $('.member-detail-footer a').click(function(e){ 
+                    e.preventDefault();
+                    
+                    //get back to the overview  
+                    obj.get('overview', search_char);
+                });
+            });         
+        });
     };
     
     //add a onclick on the tabs
     Plugin.prototype.addSearch = function () {
-    	//default vars
-    	var $elem = $(this.element),
-    		obj = this;
-    	
-    	//create new click event	
-    	$(obj.options.searchDiv).click(function(){
-			//hold an inter number to avoid double clicks.
-			var temp = 0;
-			
-			//keypres
-			$(document).keypress(function(e) {
-				if(e.which == 13 && temp == 0) {
-					temp = 1;
-	
-					//search for this value
-					obj.get('search', $("#search").val());
-					
-					//reset
-					temp = 0;
-				}
-			});
-		});
+        //default vars
+        var $elem = $(this.element),
+            obj = this;
+        
+        //create new click event    
+        $(obj.options.searchDiv).click(function(){
+            //hold an inter number to avoid double clicks.
+            var temp = 0;
+            
+            //keypres
+            $(document).keypress(function(e) {
+                if(e.which == 13 && temp == 0) {
+                    temp = 1;
+    
+                    //search for this value
+                    obj.get('search', $("#search").val());
+                    
+                    //reset
+                    temp = 0;
+                }
+            });
+        });
     };
 
     // A really lightweight plugin wrapper around the constructor,
@@ -214,38 +219,38 @@ $(function(){
 })( jQuery, window, document );
 
 ;(function ( $, window, document, undefined ) {
-	jQuery.fn.limit = function(max){
-	   var i=0;
-	   this.each(function(){
-			i++;
-			if (i > max){
-				$(this).hide();
-			}
-	   });
-	};
+    jQuery.fn.limit = function(max){
+       var i=0;
+       this.each(function(){
+            i++;
+            if (i > max){
+                $(this).hide();
+            }
+       });
+    };
 })( jQuery, window, document );
 //----------------------------------------------
 // monitor dom changes
 //----------------------------------------------
 ;(function ( $, window, document, undefined ) {
-	jQuery.fn.watch = function( id, fn ) {
-	    return this.each(function(){
-	        var self = this;
-	        var oldVal = $(self).attr(id);
-	        $(self).data(
-	            'watch_timer',
-	            setInterval(function(){
-	                if ($(self).attr(id) !== oldVal) {
-	                    fn.call(self, id, oldVal, $(self).attr(id));
-	                    oldVal = $(self).attr(id);
-	                }
-	            }, 1000)
-	        );
-	    });
-	    return self;
-	};
+    jQuery.fn.watch = function( id, fn ) {
+        return this.each(function(){
+            var self = this;
+            var oldVal = $(self).attr(id);
+            $(self).data(
+                'watch_timer',
+                setInterval(function(){
+                    if ($(self).attr(id) !== oldVal) {
+                        fn.call(self, id, oldVal, $(self).attr(id));
+                        oldVal = $(self).attr(id);
+                    }
+                }, 1000)
+            );
+        });
+        return self;
+    };
 
-	jQuery.fn.unwatch = function( id ) {
+    jQuery.fn.unwatch = function( id ) {
     return this.each(function(){
         clearInterval( $(this).data('watch_timer') );
     });
